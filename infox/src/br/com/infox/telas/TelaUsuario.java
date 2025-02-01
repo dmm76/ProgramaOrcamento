39df4d824937522e5d193c8e33b768ac3471947e
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-02-01 13:01:01 -0300

    validando os campos da telaUsuario

--- infox/src/br/com/infox/telas/TelaUsuario.java
+++ infox/src/br/com/infox/telas/TelaUsuario.java
@@ -47,11 +47,11 @@ public class TelaUsuario extends JInternalFrame {
 		setSize(482, 518); // Define o tamanho do frame
 		getContentPane().setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Id");
+		JLabel lblNewLabel = new JLabel("*Id");
 		lblNewLabel.setBounds(38, 35, 22, 14);
 		getContentPane().add(lblNewLabel);
 
-		JLabel lblNewLabel_1 = new JLabel("Nome");
+		JLabel lblNewLabel_1 = new JLabel("*Nome");
 		lblNewLabel_1.setBounds(21, 206, 46, 14);
 		getContentPane().add(lblNewLabel_1);
 
@@ -59,16 +59,16 @@ public class TelaUsuario extends JInternalFrame {
 		lblNewLabel_2.setBounds(21, 255, 46, 14);
 		getContentPane().add(lblNewLabel_2);
 
-		JLabel lblNewLabel_3 = new JLabel("Login");
+		JLabel lblNewLabel_3 = new JLabel("*Login");
 		lblNewLabel_3.setBounds(21, 90, 46, 14);
 		getContentPane().add(lblNewLabel_3);
 
-		JLabel lblNewLabel_4 = new JLabel("Senha");
+		JLabel lblNewLabel_4 = new JLabel("*Senha");
 		lblNewLabel_4.setBounds(21, 139, 46, 14);
 		getContentPane().add(lblNewLabel_4);
 
-		JLabel lblNewLabel_5 = new JLabel("Perfil");
-		lblNewLabel_5.setBounds(176, 34, 46, 14);
+		JLabel lblNewLabel_5 = new JLabel("*Perfil");
+		lblNewLabel_5.setBounds(186, 35, 46, 14);
 		getContentPane().add(lblNewLabel_5);
 
 		txtUserId = new JTextField();
@@ -102,6 +102,12 @@ public class TelaUsuario extends JInternalFrame {
 		getContentPane().add(cbxPerfil);
 
 		JButton btnUserAdd = new JButton("");
+		btnUserAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// chamando o metodo adicionar
+				adicionar();
+			}
+		});
 		btnUserAdd.setToolTipText("Adicionar");
 		btnUserAdd.setIcon(new ImageIcon(TelaUsuario.class.getResource("/br/com/infox/icones/adicionar.png")));
 		btnUserAdd.setBounds(10, 393, 89, 52);
@@ -131,6 +137,10 @@ public class TelaUsuario extends JInternalFrame {
 		btnUserDelete.setBounds(367, 393, 89, 52);
 		getContentPane().add(btnUserDelete);
 
+		JLabel lblNewLabel_6 = new JLabel("*Campos Obrigatórios");
+		lblNewLabel_6.setBounds(21, 319, 131, 14);
+		getContentPane().add(lblNewLabel_6);
+
 		// Centralizando no JDesktopPane
 		if (desktopPane != null) {
 			int x = (desktopPane.getWidth() - getWidth()) / 2;
@@ -139,6 +149,14 @@ public class TelaUsuario extends JInternalFrame {
 		}
 		// Chama o modulo de conexao com o banco
 		conexao = ModuloConexao.conector();
+	}
+
+	private void limparTela() {
+		txtUserName.setText("");
+		txtUserPhone.setText("");
+		txtUserLogin.setText("");
+		txtUserPassword.setText("");
+		//cbxPerfil.setSelectedIndex(-1);
 	}
 
 	private void consultar() {
@@ -153,17 +171,48 @@ public class TelaUsuario extends JInternalFrame {
 				txtUserPhone.setText(rs.getString(3));
 				txtUserLogin.setText(rs.getString(4));
 				txtUserPassword.setText(rs.getString(5));
-				//a linha refere-se ao combobox
+				// a linha refere-se ao combobox
 				cbxPerfil.setSelectedItem(rs.getString(6));
 			} else {
 				JOptionPane.showMessageDialog(null, "Usuário não encontrado!");
-				txtUserName.setText("");
-		        txtUserPhone.setText("");
-		        txtUserLogin.setText("");
-		        txtUserPassword.setText("");
-		        cbxPerfil.setSelectedIndex(-1); // Define como nenhum item selecionado
+				limparTela();
 			}
 
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+
+	}
+
+	private void adicionar() {
+		String sql = "insert into tbusuarios(iduser, usuario, fone, login, senha, perfil) values(?,?,?,?,?,?)";
+		try {
+			pst = conexao.prepareStatement(sql);
+			pst.setString(1, txtUserId.getText());
+			pst.setString(2, txtUserName.getText());
+			pst.setString(3, txtUserPhone.getText());
+			pst.setString(4, txtUserLogin.getText());
+			pst.setString(5, txtUserPassword.getText());
+			pst.setString(6, cbxPerfil.getSelectedItem().toString());
+			System.out.println(cbxPerfil.getSelectedItem().toString());
+			// Validacao dos campos obrigatorios
+			if ((txtUserId.getText().isEmpty()) || (txtUserName.getText().isEmpty()) || (txtUserLogin.getText().isEmpty()) || (txtUserPassword.getText().isEmpty())) {
+				JOptionPane.showMessageDialog(null, "Preencha todos os campos obrigatórios!");
+				limparTela();
+			} else {
+				// a linha abaixo atualiza a tabela de usuario com os dados do formulario
+				// Atualiza a tabela com os dados do formulário
+				// pst.executeUpdate();
+				// modificado para ao executar exibir uma mensagem caso tudo ocorreu bem
+				int adicionado = pst.executeUpdate();
+				// maior que zero pq ao adicionar uma (1) linha o programa retorna 1
+				System.out.println(adicionado);
+				if (adicionado > 0) {
+					JOptionPane.showMessageDialog(null, "Usuário adicionado com sucesso!");
+					txtUserId.setText("");
+					limparTela();
+				}
+			}
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e);
 		}
