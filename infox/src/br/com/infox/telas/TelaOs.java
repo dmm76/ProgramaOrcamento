7e646dc5bfaf17d0f11a4c93152300fb7d71547e
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-02-04 18:05:36 -0300

    funcionalidades do botao pesquisar e metodo

--- infox/src/br/com/infox/telas/TelaOs.java
+++ infox/src/br/com/infox/telas/TelaOs.java
@@ -44,7 +44,6 @@ public class TelaOs extends JInternalFrame {
 	ResultSet rs = null;
 
 	// variavel armazenar um texto dependendo do radio button selecionado
-	
 
 	private static final long serialVersionUID = 1L;
 	private JTextField textNumeroOs;
@@ -57,25 +56,40 @@ public class TelaOs extends JInternalFrame {
 	private JTextField txtServico;
 	private JTextField txtTecnico;
 	private JTextField txtValor;
+	private JComboBox<String> cbxSituacao;
+
+	// Variaveis usadas para a marcação dos radiobuttons
+
+	// Variavel que armazena o texto do radiobutton selecionado
+	String tipo = "";
+
+	// Inicializando os RadioButtons antes de adicioná-los ao frame
+	JRadioButton rdbOrcamento = new JRadioButton("Orçamento");
+	JRadioButton rdbOrdem = new JRadioButton("Ordem de Serviço");
+	ButtonGroup grupo = new ButtonGroup();
 	
-	//Variaveis usadas para a marcação dos radiobuttons
-	private JRadioButton rdbOrcamento;
-	private JRadioButton rdbOrdem;
-	private String tipo;
+	private JButton btnOsAdicionar;
+
 	
-	private JComboBox cbxSituacao;
 
 	public TelaOs() {
-		
-		// Inicializando os RadioButtons antes de adicioná-los ao frame
-        rdbOrcamento = new JRadioButton("Orçamento");
-        rdbOrdem = new JRadioButton("Ordem de Serviço");
 
-        // Adicionando os RadioButtons a um grupo para garantir seleção única
-        ButtonGroup grupo = new ButtonGroup();
-        grupo.add(rdbOrcamento);
-        grupo.add(rdbOrdem);
 		
+		rdbOrcamento.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// atribuindo um texto a variavel tipo se selecionada
+				tipo = "Orçamento";
+			}
+		});
+		
+		
+		rdbOrdem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// atribuindo um texto a variavel tipo se selecionada
+				tipo = "OS";
+			}
+		});
+
 		setTitle("Ordem de Serviços");
 		setClosable(true); // Permite fechar o JInternalFrame
 		setIconifiable(true); // Permite minimizar
@@ -89,25 +103,12 @@ public class TelaOs extends JInternalFrame {
 		JLabel lblNewLabel_1 = new JLabel("Data");
 
 		textNumeroOs = new JTextField();
+		textNumeroOs.setEditable(false);
 		textNumeroOs.setPreferredSize(new Dimension(100, 20));
 
 		textDataOs = new JTextField();
+		textDataOs.setEditable(false);
 		textNumeroOs.setPreferredSize(new Dimension(120, 20));
-
-		JRadioButton rdbOrcamento = new JRadioButton("Orçamento");
-		rdbOrcamento.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// atribuindo um texto a variavel tipo se selecionada
-				tipo = "Orçamento";
-			}
-		});
-		JRadioButton rdbOrdem = new JRadioButton("Ordem de Serviço");
-		rdbOrdem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// atribuindo um texto a variavel tipo se selecionada
-				tipo = "OS";
-			}
-		});
 
 		// Configurando o GroupLayout para o painel
 		GroupLayout panelLayout = new GroupLayout(panel);
@@ -144,13 +145,14 @@ public class TelaOs extends JInternalFrame {
 		JLabel lblNewLabel_2 = new JLabel("Situação");
 
 		cbxSituacao = new JComboBox();
-		cbxSituacao.setModel(new DefaultComboBoxModel(new String[] {"Na bancada", "Entrega OK", "Orçamento REPROVADO", "Aguardando Aprovação", "Aguradando Peças", "Abandonado pelo Cliente", "Retornou"}));
+		cbxSituacao.setModel(new DefaultComboBoxModel(new String[] { "Na bancada", "Entrega OK", "Orçamento REPROVADO",
+				"Aguardando Aprovação", "Aguradando Peças", "Abandonado pelo Cliente", "Retornou" }));
 
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Cliente",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 
-		JButton btnOsAdicionar = new JButton("");
+		btnOsAdicionar = new JButton("");
 		btnOsAdicionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				emitir_os();
@@ -161,6 +163,12 @@ public class TelaOs extends JInternalFrame {
 		btnOsAdicionar.setIcon(new ImageIcon(TelaOs.class.getResource("/br/com/infox/icones/adicionar.png")));
 
 		JButton btnOsProcurar = new JButton("");
+		btnOsProcurar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pesquisar_os();
+			}
+		});
+		
 		btnOsProcurar.setToolTipText("Procurar OS");
 		btnOsProcurar.setPreferredSize(new Dimension(75, 47));
 		btnOsProcurar.setIcon(new ImageIcon(TelaOs.class.getResource("/br/com/infox/icones/find.png")));
@@ -185,58 +193,60 @@ public class TelaOs extends JInternalFrame {
 
 		// Aplicando o GroupLayout ao conteúdo do JInternalFrame
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup().addContainerGap()
-				.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-								.addComponent(panel, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.UNRELATED)
-								.addComponent(cbxSituacao, 0, 129, Short.MAX_VALUE))
-						.addComponent(panel_2, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 447, Short.MAX_VALUE)
-						.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 447, Short.MAX_VALUE)
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(btnOsAdicionar, GroupLayout.PREFERRED_SIZE, 75,
-										GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addComponent(btnOsProcurar, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addComponent(btnOsEditar, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addComponent(btnOsRemover, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addComponent(btnOsImprimir, GroupLayout.PREFERRED_SIZE, 75, Short.MAX_VALUE)))
-				.addGap(51)));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
-						.addGroup(
-								groupLayout.createParallelGroup(Alignment.TRAILING)
-										.addGroup(groupLayout.createSequentialGroup()
-												.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-														.addComponent(lblNewLabel_2).addComponent(cbxSituacao,
-																GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-																GroupLayout.PREFERRED_SIZE))
-												.addGap(95))
-										.addComponent(panel, GroupLayout.PREFERRED_SIZE, 117,
-												GroupLayout.PREFERRED_SIZE))
-						.addPreferredGap(ComponentPlacement.RELATED)
-						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(ComponentPlacement.RELATED)
-						.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE).addGap(18)
-						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING).addGroup(groupLayout
-								.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-										.addComponent(btnOsAdicionar, GroupLayout.PREFERRED_SIZE, 47,
-												GroupLayout.PREFERRED_SIZE)
-										.addComponent(btnOsProcurar, GroupLayout.PREFERRED_SIZE, 47,
-												GroupLayout.PREFERRED_SIZE)
-										.addComponent(btnOsEditar, GroupLayout.PREFERRED_SIZE, 47,
-												GroupLayout.PREFERRED_SIZE))
-								.addComponent(btnOsRemover, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE))
-								.addComponent(btnOsImprimir, GroupLayout.PREFERRED_SIZE, 47,
-										GroupLayout.PREFERRED_SIZE))
-						.addContainerGap()));
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(22)
+									.addComponent(cbxSituacao, 0, 129, Short.MAX_VALUE))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(18)
+									.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE))))
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 447, Short.MAX_VALUE)
+						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 447, Short.MAX_VALUE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(btnOsAdicionar, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btnOsProcurar, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btnOsEditar, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btnOsRemover, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btnOsImprimir, GroupLayout.PREFERRED_SIZE, 75, Short.MAX_VALUE)))
+					.addGap(51))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblNewLabel_2)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(cbxSituacao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(67))
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnOsAdicionar, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnOsProcurar, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnOsEditar, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE))
+							.addComponent(btnOsRemover, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnOsImprimir, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
 		panel_2.setLayout(null);
 
 		JLabel lblNewLabel_5 = new JLabel("*Equipamento");
@@ -351,18 +361,23 @@ public class TelaOs extends JInternalFrame {
 		groupLayout.setAutoCreateGaps(true);
 		groupLayout.setAutoCreateContainerGaps(true);
 
-		conexao = ModuloConexao.conector();		
-		
+		conexao = ModuloConexao.conector();
+
+		// Adicionando os RadioButtons a um grupo para garantir seleção única
+		grupo.add(rdbOrcamento);
+		grupo.add(rdbOrdem);
+
 		addInternalFrameListener(new InternalFrameAdapter() {
 			@Override
 			public void internalFrameOpened(InternalFrameEvent e) {
 				// Ao abrir o form, marcar o radio button "Orçamento"
 				rdbOrcamento.setSelected(true);
-				tipo = "Orçamento"; 				
+				tipo = "Orçamento";
 			}
 		});
 
 	}
+
 	private void limpar_tela_os() {
 		txtCliPesquisar.setText("");
 		txtEquipamento.setText("");
@@ -391,8 +406,8 @@ public class TelaOs extends JInternalFrame {
 		txtCliId.setText(tblClientes.getModel().getValueAt(setar, 0).toString()); // 0 é o campo id do banco
 
 	}
-	
-	//metodo para cadastrar um OS
+
+	// metodo para cadastrar um OS
 	private void emitir_os() {
 		String sql = "insert into tbos (tipo, situacao, equipamento, defeito, servico, tecnico, valor, idcli) values (?,?,?,?,?,?,?,?)";
 		try {
@@ -404,21 +419,68 @@ public class TelaOs extends JInternalFrame {
 			pst.setString(5, txtServico.getText());
 			pst.setString(6, txtTecnico.getText());
 			pst.setString(7, txtValor.getText().replace(",", "."));
-			pst.setString(8, txtCliId.getText());		
-			
-			//Validacao dos campos obrigatorios
+			pst.setString(8, txtCliId.getText());
+
+			// Validacao dos campos obrigatorios
 			if (txtCliId.getText().isEmpty() || txtEquipamento.getText().isEmpty() || txtDefeito.getText().isEmpty()) {
 				JOptionPane.showMessageDialog(null, "Atenção preencha todos os campos obrigatórios");
 			} else {
 				int adicionado = pst.executeUpdate();
-				if(adicionado > 0) {
+				if (adicionado > 0) {
 					JOptionPane.showMessageDialog(null, "Os omitida com sucesso");
 					limpar_tela_os();
 				}
 			}
-			
+
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e);
+		}
+	}
+
+	private void pesquisar_os() {
+		String num_os = JOptionPane.showInputDialog("Número da OS");
+		String sql = "select * from tbos where os = " + num_os;
+		try {
+			pst = conexao.prepareStatement(sql);
+			rs = pst.executeQuery();
+			if (rs.next()) {
+				textNumeroOs.setText(rs.getString(1));
+				textDataOs.setText(rs.getString(2));
+				txtEquipamento.setText(rs.getString(5));
+				txtDefeito.setText(rs.getString(6));
+				txtServico.setText(rs.getString(7));
+				txtTecnico.setText(rs.getString(8));
+				txtValor.setText(rs.getString(9));
+				txtCliId.setText(rs.getString(10));
+
+				tipo = rs.getString(3); // Nome correto da coluna
+				if (tipo.equals("Orçamento")) {
+					rdbOrcamento.setSelected(true);
+					rdbOrdem.setSelected(false);
+					
+				} else if (tipo.equals("OS")) {
+					rdbOrdem.setSelected(true);
+					rdbOrcamento.setSelected(false);
+					
+				}
+
+				// 🔹 Pegando a situação no banco
+				String situacao = rs.getString("situacao"); // Nome correto da coluna
+				cbxSituacao.setSelectedItem(situacao); // Define a opção no ComboBox
+				
+				//evitando problemas
+				btnOsAdicionar.setEnabled(false);
+				txtCliPesquisar.setEnabled(false);
+				tblClientes.setVisible(false);
+
+			} else {
+				JOptionPane.showMessageDialog(null, "Os não cadastrada");
+			}
+		} catch (java.sql.SQLSyntaxErrorException e) {
+			JOptionPane.showMessageDialog(null, "OS inválida");
+			//System.out.println(e);
+		}catch(Exception e2) {
+			JOptionPane.showMessageDialog(null, e2);
 		}
 	}
 }
